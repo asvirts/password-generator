67604f0b2219f42f09efe0268953555eca1c081b
Andrew Virts <asvirts@outlook.com>
2023-05-06 17:51:53 -0500

    yes

--- index.js
+++ index.js
@@ -1,22 +1,107 @@
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
-"r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}",",",":",";","<",">",".","?",
-"/"];
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "}",
+  ",",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/"
+];
 
-generatedPasswordEl = document.getElementById("generated-password")
-
+generatedPasswordEl = document.getElementById("generated-password");
 
 // Generate Password button
 
 function getRandomCharacter(characters) {
-    return characters[Math.floor(Math.random() * characters.length)];
+  return characters[Math.floor(Math.random() * characters.length)];
 }
 
 function generatePassword(length) {
-    let password = "";
-    for (let i = 0; i < length; i++) {
-        password += getRandomCharacter(characters);
-    }
-    return password;
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += getRandomCharacter(characters);
+  }
+  return password;
 }
 
 generatedPasswordEl.textContent = generatePassword(30);
